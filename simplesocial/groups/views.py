df209fdc32a66e848ad22cfb988edb9795c967d2
Bleezer <balazssoros@gmail.com>
2017-10-11 13:30:48 +0200

    181

--- simplesocial/groups/views.py
+++ simplesocial/groups/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequiredMixin)
 
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from groups.models import Group,GroupMember
 # Create your views here.
+
+class CreateGrup(LoginRequiredMixin, generic.CreateView):
+    fields = ('name', 'description')
+    model = Group
+
+class SingleGrup(generic.DetailView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
