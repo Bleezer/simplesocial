3c3909a87a3753f95bb012ea549bf11c4b32c90f
Bleezer <balazssoros@gmail.com>
2017-10-11 13:40:33 +0200

    182

--- simplesocial/groups/admin.py
+++ simplesocial/groups/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+# Ezzel a TabularInline-nal beállítjuk hogy az Admin felületen is lehessen szerkezteni, ilyenkor már nem kell külön regisztrálni a GroupMember-t 
+class GroupMemberInline(admin.TabularInline):
+    model = models.GroupMember
+
+
+
+admin.site.register(models.Group)
